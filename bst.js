63fc93afc5204150eb191e522dc7e23c47a56c24
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-05-31 23:24:02 +0800

    feat: add delete and find method to bst

--- bst.js
+++ bst.js
@@ -1,7 +1,7 @@
 import { sortRemoveDuplicates } from "./helper.js";
 
 class Node {
-  constructor(data) {
+  constructor(data = null) {
     this.data = data;
     this.left = null;
     this.right = null;
@@ -29,6 +29,7 @@ class Tree {
 
   insert(value, root = this.root) {
     if (root === null) return new Node(value);
+    if (!root.left && !root.right) return null;
 
     if (root === value) {
       return root;
@@ -43,6 +44,53 @@ class Tree {
     }
 
     return root;
+  }
+
+  deleteValue(value, root = this.root) {
+    if (root == null) return root;
+
+    if (root.data > value) {
+      root.left = this.deleteValue(value, root.left);
+    } else if (root.data < value) {
+      root.right = this.deleteValue(value, root.right);
+    } else {
+      //delete leap node
+      if (!root.left && !root.right) return null;
+      //delete node with 1 single child
+      if (!root.left ^ !root.right) {
+        if (root.left) {
+          root.data = this.deleteValue(value, root.left);
+        } else if (root.right) {
+          root.data = this.deleteValue(value, root.right);
+        }
+        return root.data;
+      }
+
+      //delete node with 2 child
+      if (root.left && root.right) {
+        let rightSubtree = root.right;
+        while (rightSubtree.left !== null && rightSubtree !== null) {
+          rightSubtree = rightSubtree.left;
+        }
+
+        root.data = rightSubtree.data;
+
+        root.right = this.deleteValue(root.data, root.right);
+      }
+    }
+    console.log(root);
+    return root;
+  }
+
+  find(value, root = this.root) {
+    if (value === root.data) return root;
+
+    if (root.data < value) {
+      return this.find(value, root.right);
+    }
+    if (root.data > value) {
+      return this.find(value, root.left);
+    }
   }
 }
 
@@ -59,8 +107,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const tree = new Tree([1, 1, 4, 5, 4, 3, 3, 3, 3, 6, 7]);
-tree.insert(2);
+const tree = new Tree([1, 1, 4, 5, 4, 3, 3, 3, 3, 6, 7, 2]);
 
-console.log(tree.root);
+console.log(tree.find(2));
 prettyPrint(tree.root);
